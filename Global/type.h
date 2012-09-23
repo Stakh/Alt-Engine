c1e8f765a6f1f973bc7aece8e8730584c801b177
Sapin <sapinerie@gmail.com>
2012-09-23 11:27:18 +0200

    Add compound types in Global/type.h

--- Global/type.h
+++ Global/type.h
@@ -5,6 +5,8 @@
 
 #include "system.h"
 
+// Basic type(s)
+
 typedef int8_t   i8;
 typedef uint8_t  u8;
 
@@ -20,8 +22,38 @@ typedef float  real;
 typedef double real;
 #endif
 
+// Logic type(s)
+
 typedef char   boolean;
 #define true  1
 #define false 0
+
+// Compound type(s)
+
+typedef struct
+{
+    u32 x;
+    u32 y;
+} Coordinate2;
+
+typedef struct
+{
+    real x;
+    real y;
+} Vector2;
+
+typedef struct
+{
+    u32 x;
+    u32 y;
+    u32 z;
+} Coordinate3;
+
+typedef struct
+{
+    real x;
+    real y;
+    real z;
+} Vector3;
 
 #endif // TYPE_H
